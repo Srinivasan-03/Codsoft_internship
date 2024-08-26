b32ff0f9863f376f6efc458ce758e1278f466ab5
Srinivasan-03 <srinivasan.sivananthan@gmail.com>
2024-08-26 18:57:44 +0530

    XO_game.cpp

--- XO_game.cpp
+++ XO_game.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 char board[3][3] = {{'1', '2', '3'}, 
                     {'4', '5', '6'}, 
                     {'7', '8', '9'}};
@@ -13,14 +12,12 @@ void displayBoard() {
     cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
 }
 bool checkWin() {
-    // Check rows and columns
     for (int i = 0; i < 3; i++) {
         if ((board[i][0] == board[i][1] && board[i][1] == board[i][2]) ||
             (board[0][i] == board[1][i] && board[1][i] == board[2][i])) {
             return true;
         }
     }
-    // Check diagonals
     if ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
         (board[0][2] == board[1][1] && board[1][1] == board[2][0])) {
         return true;
@@ -41,7 +38,6 @@ void playerMove() {
     int move;
     cout << "Player " << currentPlayer << ", enter your move (1-9): ";
     cin >> move;
-    // Map 1-9 input to board positions
     int row = (move - 1) / 3;
     int col = (move - 1) % 3;
     if (move >= 1 && move <= 9 && board[row][col] != 'X' && board[row][col] != 'O') {
